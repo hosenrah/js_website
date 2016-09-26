fbb87d920f41acecf0f011c9d8d12511372344d4
Andreas Rau <andreas.rau@de.ibm.com>
2016-09-26 23:04:41 +0200

    :lipstick: changed execution order of cmds

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    scp -r . u74789873@s493274694.online.de:js_website
+    scp -r bower_components u74789873@s493274694.online.de:js_website/bower_components
+    scp -r node_modules u74789873@s493274694.online.de:js_website/node_modules
+    scp -r js u74789873@s493274694.online.de:js_website/js
+    scp index.html u74789873@s493274694.online.de:js_website/index.html
     echo "Deployed to Webserver."
 else
     echo "Not deploying, since this branch isn't master."
