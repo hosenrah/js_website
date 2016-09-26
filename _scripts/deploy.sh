859dab3f2fba43739f58f1e34d5b48d2c661613a
Andreas Rau <andreas.rau@de.ibm.com>
2016-09-26 22:56:42 +0200

    :lipstick: improved install script, now deletes unnecessary files

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    scp -r . u74789873@s493274694.online.de:
+    scp -r . u74789873@s493274694.online.de:js_website
     echo "Deployed to Webserver."
 else
     echo "Not deploying, since this branch isn't master."
--- _scripts/install.sh
+++ _scripts/install.sh
@@ -6,3 +6,9 @@ openssl aes-256-cbc -K $encrypted_f68595d96691_key -iv $encrypted_f68595d96691_i
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
+# remove unnecessary files
+rm .git
+rm .gitignore
+rm .travis.yml
+rm README.md
+rm -r _scripts
