5e04f108c5c9a4237f9a4b4acec9442320f63caa
Andreas Rau <andreas.rau@de.ibm.com>
2016-09-26 22:40:53 +0200

    :lipstick: fixed bug in install.sh script

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_f68595d96691_key -iv $encrypted_f68595d96691_iv -in id_rsa.enc -out id_rsa -d
+openssl aes-256-cbc -K $encrypted_f68595d96691_key -iv $encrypted_f68595d96691_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
