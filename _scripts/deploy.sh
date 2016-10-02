f458145e0d1942d05f1a8c400e7d43c96f303c0b
Andreas Rau <andreas.rau93@gmail.com>
2016-10-02 23:46:28 +0200

    :lipstick: added js files locally

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    scp -r bower_components u74789873@s493274694.online.de:js_website/bower_components
-    scp -r node_modules u74789873@s493274694.online.de:js_website/node_modules
     scp -r js u74789873@s493274694.online.de:js_website/js
     scp -r css u74789873@s493274694.online.de:js_website/css
-    scp -r img u74789873@s493274694.online.de:js_website/img
-    
-    
+    scp -r img u74789873@s493274694.online.de:js_website/img    
     scp index.html u74789873@s493274694.online.de:js_website/index.html
     echo "Deployed to Webserver."
 else
