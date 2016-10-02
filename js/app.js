ed2a65655764f9491db531bfd1a276002ffbdb22
Andreas Rau <andreas.rau93@gmail.com>
2016-10-02 13:12:09 +0200

    :sparkles: changed grid framework to skeletonCSS and improved main vimeo video responsiveness, it is awesome now

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 var js_website = angular.module('js_website', ['ngRoute', 'ngResource', 'jtt_vimeo']);
 
 // CONTROLLERS
+js_website.controller('homeController', ['$scope', function($scope) {
+
+    
+}]);
+
+js_website.controller('instagramController', ['$scope', '$resource', function($scope, $resource) {
+    
+    $scope.instagramAPI = $resource("https://api.instagram.com/v1/users/self/media/recent/?access_token=2235700352.4ee6f6e.b902f5c21acc4655a71b7043b21cdcfc", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.instagramFeed = $scope.instagramAPI.get();
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
+    
+}]);
+
 js_website.controller('vimeoController', ['$scope', '$resource', '$sce', 'vimeoFactory', function($scope, $resource, $sce, vimeoFactory) {
-    
-//    $scope.vimeoAPI = $resource("https://api.vimeo.com/me/videos?authorization=bearer+1bb5838a1c16bdab1e8eac3add1b6f2a");
-//    
-//    $scope.vimeoFeed = $scope.vimeoAPI.get();
-    
     $scope.vimeoFeed = [];
     
     $scope.mainVimeoVideo;
@@ -49,7 +63,7 @@ js_website.controller('vimeoController', ['$scope', '$resource', '$sce', 'vimeoF
         }
     };
     
-    $scope.getIframeSrc = function(src) {
+    $scope.getVimeoVideos = function(src) {
         var videoHash = src.slice(8);
         return $sce.trustAsResourceUrl('https://player.vimeo.com/video/' + videoHash + '?badge=0&autopause=1&player_id=0');
     };
@@ -70,40 +84,26 @@ js_website.controller('vimeoController', ['$scope', '$resource', '$sce', 'vimeoF
 
 }]);
 
-js_website.controller('instagramController', ['$scope', '$resource', function($scope, $resource) {
-    
-    $scope.instagramAPI = $resource("https://api.instagram.com/v1/users/self/media/recent/?access_token=2235700352.4ee6f6e.b902f5c21acc4655a71b7043b21cdcfc", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-    
-    $scope.instagramFeed = $scope.instagramAPI.get();
-    
-    $scope.convertToDate = function(dt) { 
-      
-        return new Date(dt * 1000);
-        
-    };
-    
-}]);
-
-js_website.controller('homeController', ['$scope', function($scope) {
-    
-}]);
-
-
 //// ROUTES
-//js_website.config(function ($routeProvider) {
-//   
-//    $routeProvider
-//    
-//    .when('/', {
-//        templateUrl: 'pages/home.html',
-//        controller: 'homeController'
-//    })
-//    
-//    .when('/latest', {
-//        templateUrl: 'pages/latest.html',
-//        controller: 'instagramController'
-//    })
-//});
+js_website.config(function ($routeProvider) {
+   
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'vimeoController'
+    })
+    
+    .when('/latest', {
+        templateUrl: 'pages/latest.html',
+        controller: 'instagramController'
+    })
+    
+    .when('/videos', {
+        templateUrl: 'pages/videos.html',
+        controller: 'vimeoController'
+    })
+});
 
 // DIRECTIVES
 js_website.directive("instagramPost", function() {
